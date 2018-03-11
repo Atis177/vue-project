f6857c26cce8e7b98027522a36f4d3c33d7f31b0
ashalagina <ashalagina@oggettoweb.com>
2018-03-11 21:57:09 +0300

    change port

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
-var port = Number(process.env.PORT || 3004);
+var jsonPort = Number(process.env.PORT || 3004);
 server.use(middlewares);
 server.use(router);
-server.listen(port, function () {
+server.listen(jsonPort, function () {
     console.log('JSON Server is running')
 });
